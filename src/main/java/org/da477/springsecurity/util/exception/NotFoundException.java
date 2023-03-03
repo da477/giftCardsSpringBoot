cb00680899042ab97d8a31719a1cbd70a3f9b12a
da477 <da.1cint.eu@gmail.com>
2023-03-03 17:15:05 +0200

    add GiftCards: working controller, doesn't work auth (3)

---
+++ src/main/java/org/da477/springsecurity/util/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package org.da477.springsecurity.util.exception;
+
+public class NotFoundException  extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
